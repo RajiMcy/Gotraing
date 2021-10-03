8171ea170c7e0de221f0e6fca53214705f929dd6
Rajesh <mmsit92@gmail.com>
2021-10-03 18:31:35 +0530

    tod's example

---
+++ error_handling/tod2/1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("names.txt")
+	if err != nil {
+
+		fmt.Println(err)
+
+		return
+
+	}
+
+	defer f.Close()
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+
+		fmt.Println(err)
+
+		return
+
+	}
+
+	fmt.Println(string(b))
+}
